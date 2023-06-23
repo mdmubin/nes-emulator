da4025b30334d99206864d1511ee89cc32fd7cca
Mubin <mdmubin.xyz@gmail.com>
2023-06-23 12:55:21 +0600

    added raylib dependency

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,13 @@
+#include <raylib.h>
 
 int main() {
+    InitWindow(500, 500, "NES Emulator");
 
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+            ClearBackground(BLACK);
+            DrawText("Hello Raylib!", 10, 10, 13, YELLOW);
+        EndDrawing();
+    }
 }
