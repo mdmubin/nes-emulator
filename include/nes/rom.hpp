206aa3dd05fa54e95eb05760c393b044a2b46e46
mdmubin <mdmubin.xyz@gmail.com>
2023-07-17 13:42:19 +0600

    changed rom interface and added constructor

--- include/nes/rom.hpp
+++ include/nes/rom.hpp
@@ -9,21 +9,35 @@ namespace nes {
 
 class Rom {
 public:
-    Rom(const char *filepath);
+    Rom(const char *romFile);
+
+    inline bool is_ok() const {
+        return romStatusOk;
+    }
 
 private:
-    enum ScreenMirroring {
-        VERTICAL,
-        HORIZONTAL,
-        FOUR_SCREEN,
-    };
 
-    ScreenMirroring mirror;
+
+    typedef enum ScreenMirroring {
+        sm_UNDEFINED,
+        sm_VERTICAL,
+        sm_HORIZONTAL,
+        sm_FOUR_SCREEN,
+        sm_SINGLE_SCREEN, // may remain unused
+    } sm;
+
+    typedef enum Mapper {
+        mp_UNDEFINED,
+        mp_NROM,
+    } mp;
+
+    mp mapper = mp_UNDEFINED;
+    sm mirror = sm_UNDEFINED;
 
     std::vector<u8> prgMem;
     std::vector<u8> chrMem;
 
-    u8 mapper;
+    bool romStatusOk = false;
 };
 
 } // namespace nes
--- source/nes/rom.cpp
+++ source/nes/rom.cpp
@@ -1,36 +1,67 @@
 #include "nes/rom.hpp"
 
 #include <fstream>
+#include <raylib.h>
+#include <fmt/core.h>
 
 using namespace nes;
 
-struct FileHeader {
-    char ftype[4];
-    u8 prgRomSize;
-    u8 chrRomSize;
-    u8 mapperFlags1;
-    u8 mapperFlags2;
-    u8 PrgRamSize;
-    u8 systemFlags1;
-    u8 systemFlags2;
-    char padding[5];
-};
+//
 
-Rom::Rom(const char *filepath) {
-    std::ifstream rom(filepath, std::ios::binary);
+constexpr u32 CHR_ROM_PAGE_SIZE =  8 * 1024;
+constexpr u32 PRG_ROM_PAGE_SIZE = 16 * 1024;
 
-    if (rom.is_open()) {
-        FileHeader h = {0};
-        rom.read((char *) (&h), sizeof(h));
+//
 
-        bool iNesFmt = false;
-        if (h.ftype[0] == 'N' && h.ftype[1] == 'E' && h.ftype[2] == 'S' && h.ftype[3] == 0x1A) {
-            iNesFmt = true;
+Rom::Rom(const char *romFile) {
+    std::ifstream rom(romFile, std::ios::binary);
+
+    if (rom.good()) {
+        u8 header[16];
+        rom.read((char *) header, sizeof(header));
+
+        // check file format
+        if (header[0] == 0x4E && // 'N'
+            header[1] == 0x45 && // 'E'
+            header[2] == 0x53 && // 'S'
+            header[3] == 0x1A    // EOF
+        ) {
+            if (header[7] & 0x0C) {
+                TraceLog(LOG_ERROR, "NES 2.0 Rom not supported");
+                return;
+            }
+        } else {
+            TraceLog(LOG_ERROR, "Unrecognized Rom file format");
+            return;
         }
 
-        bool iNes20Fmt = false;
-//        if (iNesFmt && (h.mapperFlags2 & )) {
-//            iNesFmt = true;
-//        }
+        u32 prgRomSize = header[4] * PRG_ROM_PAGE_SIZE;
+        u32 chrRomSize = header[5] * CHR_ROM_PAGE_SIZE;
+
+        bool fourScreen = (header[6] & 0x08) != 0;
+        bool vertMirror = (header[6] & 0x01) == 1;
+
+        mirror = fourScreen ? sm_FOUR_SCREEN : (vertMirror ? sm_VERTICAL : sm_HORIZONTAL);
+
+        // check for trainer data, ignoring it for now
+        if (header[6] & 0x04) {
+            rom.seekg(512, std::ios::cur);
+        }
+
+        // get mapper id and set mapper. TODO: implement mapper and set mapper here
+        u8 mapperId = (header[7] & 0xF0) | (header[6] >> 4);
+
+        // read rom data
+
+        prgMem.resize(prgRomSize);
+        rom.read((char *) (&prgMem[0]), prgRomSize);
+
+        chrMem.resize(chrRomSize);
+        rom.read((char *) (&chrMem[0]), chrRomSize);
+
+        rom.close();
+        romStatusOk = true;
+    } else {
+        TraceLog(LOG_ERROR, fmt::format("Failed to open rom file: {}", romFile).c_str());
     }
 }
